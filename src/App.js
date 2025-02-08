25407269db0afb6e27d3efbfe0c33b34739aa72a
Cristian A Vittini <140289468+Cvittini@users.noreply.github.com>
2025-02-07 22:02:52 -0500

    add header with logo and navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Offers from "./Pages/Offers";
 import Profile from "./Pages/Profile";
 import Signin from "./Pages/Signin";
 import SignUp from "./Pages/SignUp";
 import ForgotPassword from "./Pages/ForgotPassword";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
+    <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Offers" element={<Offers />} />
         <Route path="/Profile" element={<Profile />} />
-        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Sign-in" element={<Signin />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />} />
       </Routes>
+      </Router>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";*/
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
 
 reportWebVitals();
